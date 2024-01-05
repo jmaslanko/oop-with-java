50755cdfc472a04a2fa88d7b8d10d8685e55afe6
Jeremy Maslanko <maslankoj@gmail.com>
2024-01-05 15:18:09 -0500

    update folder paths

---
+++ Module2/BackToBasics/ConstantTest.java
@@ -0,0 +1,13 @@
+// A quick test to show how final works when declaring variables
+// that are unable to change
+
+public class ConstantTest {
+    public static void main(String[] args) {
+        // final double PI = 3.14159265359;
+        // // PI = 3.14;
+        // System.out.println(PI);
+        double avg = 4.0;
+        int gpa = (int)avg;
+        System.out.println(gpa);
+    }
+}
